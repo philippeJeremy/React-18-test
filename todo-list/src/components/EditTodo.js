a4ef34d4c4fcc830a2892f73f0e447b7b29be994
jeremy <jeremy561000@gmail.com>
2023-02-14 20:29:30 +0100

    todo test

---
+++ todo-list/src/components/EditTodo.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function EditTodo({ todo, editTodo, cancelEditTodo }) {
+  const [value, setValue] = useState(todo.content);
+
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  }
+
+  function handleKeyDow(e) {
+    if (e.code === "Enter" && value.length) {
+      editTodo(value);
+      setValue("");
+    }
+  }
+
+  function handleClick() {
+    if (value.length) {
+      editTodo(value);
+      setValue("");
+    }
+  }
+
+  return (
+    <div className="d-flex flex-row justify-content-center align-items-center mb-10">
+      <input
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyDow}
+        value={value}
+        placeholder="Ajouter une todo"
+        className="mr-15 flex-fill"
+      />
+      <button
+        onClick={cancelEditTodo}
+        className="btn btn-reverse-primary mr-15"
+      >
+        {" "}
+        Annuler
+      </button>
+      <button onClick={handleClick} className="btn btn-primary">
+        {" "}
+        Sauvegarder
+      </button>
+    </div>
+  );
+}
+
+export default EditTodo;
---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,47 @@
+export default function TodoItem({
+  todo,
+  deleteTodo,
+  toggleTodo,
+  editTodo,
+  selectTodo,
+}) {
+  return (
+    <li
+      onClick={selectTodo}
+      className={`mb-10 d-flex flex-row justify-content-center align-items-center p-10 ${
+        todo.selected ? "selected" : ""
+      }  `}
+    >
+      <span className="flex-fill">
+        {todo.content} {todo.done && "✅"}
+      </span>
+      <button
+        className="btn btn-primary mr-15"
+        onClick={(e) => {
+          e.stopPropagation();
+          toggleTodo();
+        }}
+      >
+        Valider
+      </button>
+      <button
+        className="btn btn-primary mr-15"
+        onClick={(e) => {
+          e.stopPropagation();
+          editTodo();
+        }}
+      >
+        Modifier
+      </button>
+      <button
+        className="btn btn-reverse-primary"
+        onClick={(e) => {
+          e.stopPropagation();
+          deleteTodo();
+        }}
+      >
+        Supprimer
+      </button>
+    </li>
+  );
+}
